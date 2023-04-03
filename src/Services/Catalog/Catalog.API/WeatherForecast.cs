4d76776ffffce0d85475810d0432475f994fdcf6
muhammad-abdulloh <clevercoderr@gmail.com>
2023-04-03 08:01:39 +0500

    Create Catalog.API

---
+++ src/Services/Catalog/Catalog.API/WeatherForecast.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace Catalog.API
+{
+    public class WeatherForecast
+    {
+        /// <summary>
+        /// datetime
+        /// </summary>
+        public DateTime Date { get; set; }
+
+        /// <summary>
+        ///  temperatur
+        /// </summary>
+
+        public int TemperatureC { get; set; }
+
+        /// <summary>
+        /// faradey
+        /// </summary>
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        /// <summary>
+        /// xulosa desayam bo'lo'ras kerak
+        /// </summary>
+
+        public string Summary { get; set; }
+    }
+}
